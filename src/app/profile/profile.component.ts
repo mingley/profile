4c2042a083034cb077a3d459b6e6ecdd50859d36
Michael Ingley <ming@Michaels-MacBook-Pro.local>
2020-01-17 12:53:51 -0800

    done

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { APIService } from '../API.service';
 import { User } from '../user';
 import { Auth } from 'aws-amplify';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -12,29 +13,79 @@ import { Auth } from 'aws-amplify';
 export class ProfileComponent implements OnInit {
   userId: string;
   userName: string;
-  user = new User('', '', '','','','');
+  user = new User('', '', '', '', '', '');
+  showPhoto: boolean;
+  userCreated: boolean;
 
-  constructor(private api: APIService) { }
+  constructor(
+    private api: APIService,
+    private router: Router
+    ) { }
 
-  ngOnInit() {
-    Auth.currentAuthenticatedUser({
-      bypassCache: false
-    }).then(async user => {
-      this.userId = user.attributes.sub;
-      this.userName = user.username;
-    })
-    .catch(err => console.log(err));
-  }
-
-  async updateProfile(){
+    ngOnInit() {
+      this.showPhoto = false;
+      Auth.currentAuthenticatedUser({
+          bypassCache: false
+        }).then(async user => {
+          this.userName = user.username;
+          this.userId = user.attributes.sub;
+          let result = await this.api.GetUser(this.userId);
+          if (!result) {
+            this.userCreated = false;
+            this.user = new User('', '', '', '', '', '');
+          } else {
+            this.userCreated = true;
+            this.showPhoto = !!result.image;
+            this.user = new User(
+              this.userId,
+              result.username,
+              result.firstName,
+              result.lastName,
+              result.bio,
+              result.image
+            )
+          }
+        })
+        .catch(err => console.log(err));
+    }
+  
+  async updateProfile() {
     const user = {
       id: this.userId,
-      username: this.user.firstName + '' + this.user.lastName,
+      username: this.user.firstName + '_' + this.user.lastName,
       firstName: this.user.firstName,
       lastName: this.user.lastName,
-      bio: this.user.aboutMe
+      bio: this.user.aboutMe,
+      image: this.user.imageUrl
     }
-    await this.api.CreateUser(user);
+    await this.api[this.getType()](user);
+  }
+  
+  async onImageUploaded(e) {
+    this.user.imageUrl = e.key;
+    if (this.userCreated) {
+      await this.api.UpdateUser({
+        id: this.userId,
+        image: this.user.imageUrl
+      });
+    }
+    this.showPhoto = true;
+  }
+
+  editPhoto() {
+    this.showPhoto = false;
+  }
+
+  getType(): string {
+    return this.userCreated ? 'UpdateUser' : 'CreateUser';
+  }
+
+  logOut() {
+    Auth.signOut({ global: true})
+     .then(data => {
+       this.router.navigate(['/auth']);
+     })
+     .catch(err => console.log(err));
   }
 
 }
